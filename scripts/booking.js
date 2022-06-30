eb563fe39092abe73e0ed5a031377f61e91033be
MatthewSia98 <71731492+MatthewSia98@users.noreply.github.com>
2022-06-30 09:49:37 -0400

    Delete scripts directory

--- scripts/booking.js
+++
@@ -1,4 +0,0 @@
-function load_confirmation(elem) {
-    const email = document.getElementById("emailInput").value;
-    localStorage.setItem('email', email);
-}
--- scripts/doctors.js
+++
@@ -1,4 +0,0 @@
-function load_profile(elem) {
-    const doctorName = elem.parentElement.parentElement.parentElement.parentElement.id;
-    localStorage.setItem('doctorName', doctorName);
-}
